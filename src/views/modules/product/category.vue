467b6b7a775b57a535dde0bb8cc5d0b505cc35a0
roydonGuo <3133010060@qq.com>
2023-04-05 22:41:35 +0800

    init

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>category</h1>
+  </div>
+</template>
+
+<script>
+//import 《组件名称》 from '《组件路径》'
+
+export default {
+  components: {},
+  props: {},
+  data() {
+    //数据
+    return {};
+  },
+  //计算属性
+  computed: {},
+  watch: {},
+  methods: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() { },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() { },
+  beforeCreate() { }, //生命周期 - 创建之前
+  beforeMount() { }, //生命周期 - 挂载之前
+  beforeUpdate() { }, //生命周期 - 更新之前
+  updated() { }, //生命周期 - 更新之后
+  beforeDestroy() { }, //生命周期 - 销毁之前
+  destroyed() { }, //生命周期 - 销毁完成
+  activated() { } //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style scoped></style>
